c1007f1944463dde88245195e05922a1da22dd3c
lsongzhi <lsongzhi@163.com>
2018-10-11 21:40:52 +0800

    keep parser immutable

--- src/parser.rs
+++ src/parser.rs
@@ -1,99 +1,99 @@
+use std::fmt::{Display, Error, Formatter};
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::BufReader;
-use std::fmt::{Display, Formatter, Error};
+use std::io::prelude::*;
 
+#[derive(Clone)]
 enum CommentToken {
     Common(String),
     MultiLine(String, String),
 }
 
-enum LineType {
-    Blank,
-    Comment,
-    Code,
+struct CommonParser {
+    path: String,
+    comment_tokens: Vec<CommentToken>,
 }
 
-struct CommonParser {
-    reader: BufReader<File>,
-    comment_tokens: Vec<CommentToken>,
-    is_in_comment: bool,
-    being_in_multiline_token: Option<CommentToken>,
-    blank_count: u64,
-    comment_count: u64,
-    code_count: u64,
+#[derive(Copy, Clone, Debug)]
+struct CodeStats {
+    code: u64,
+    blank: u64,
+    comment: u64,
 }
 
 trait Parser {
-    fn parse(&mut self);
-    fn parse_line(&mut self, line: &str);
+    fn parse(self) -> CodeStats;
+    fn parse_line(&self, line: &str, stats: &mut CodeStats, being_in_multiline_token: &mut Option<CommentToken>);
 }
 
 impl CommonParser {
-    fn new(file: File, comment_tokens: Vec<CommentToken>) -> Self {
+    fn new(path: &str, comment_tokens: Vec<CommentToken>) -> Self {
         Self {
-            reader:BufReader::new(file),
+            path: path.to_string(),
             comment_tokens,
-            is_in_comment: false,
-            being_in_multiline_token: None,
-            blank_count: 0,
-            comment_count: 0,
-            code_count: 0,
         }
     }
 }
 
-impl Display for CommonParser {
+impl Display for CodeStats {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "{{ Code: {}, Comment: {}, Blank: {} }}", self.code_count, self.comment_count, self.blank_count)
+        write!(f, "{{ Code: {}, Comment: {}, Blank: {} }}", self.code, self.comment, self.blank)
     }
 }
 
 impl Parser for CommonParser {
-    fn parse(&mut self) {
-        for line in self.reader.lines() {
+    fn parse(self) -> CodeStats {
+        let file = File::open(self.path.as_str()).unwrap();
+        let reader = BufReader::new(file);
+        let mut stats = CodeStats {
+            code: 0u64,
+            blank: 0u64,
+            comment: 0u64,
+        };
+        let mut being_in_multiline_token: Option<CommentToken> = None;
+        for line in reader.lines() {
             if let Ok(s) = line {
-                self.parse_line(s.as_str())
+                self.parse_line(s.as_str(), &mut stats, &mut being_in_multiline_token);
             }
         }
+        stats
     }
-    fn parse_line(&mut self, line: &str) {
-        match self.being_in_multiline_token {
-            Some(ref comment_token) => {
+    fn parse_line(&self, line: &str, stats: &mut CodeStats, being_in_multiline_token: &mut Option<CommentToken>) {
+        match being_in_multiline_token {
+            Some(_comment_token) => {
                 for ref token in self.comment_tokens.iter() {
-                    if let CommentToken::MultiLine(s, e) = token {
-                        if line.ends_with(e.as_str()){
-                            self.comment_count += 1;
-                            self.is_in_comment = false;
+                    if let CommentToken::MultiLine(_s, e) = token {
+                        if line.ends_with(e.as_str()) {
+                            stats.comment += 1;
                             return;
                         }
                     }
                 }
-                self.comment_count += 1;
+                stats.comment += 1;
             }
             None => {
                 if line.trim().is_empty() {
-                    self.blank_count += 1;
+                    stats.blank += 1;
                     return;
                 }
-                for &token in self.comment_tokens.iter() {
+                for ref token in self.comment_tokens.iter() {
                     match token {
                         CommentToken::Common(s) => {
                             if line.starts_with(s.as_str()) {
-                                self.comment_count += 1;
+                                stats.comment += 1;
                                 return;
                             }
                         }
-                        CommentToken::MultiLine(s, e) => {
+                        CommentToken::MultiLine(s, _e) => {
                             if line.starts_with(s.as_str()) {
-                                self.comment_count += 1;
-                                self.being_in_multiline_token = Some(token);
+                                stats.comment += 1;
+                                *being_in_multiline_token = Some((**token).clone());
                                 return;
                             }
                         }
                     }
                 }
-                self.code_count += 1;
+                stats.code += 1;
             }
         }
     }
@@ -102,13 +102,13 @@ impl Parser for CommonParser {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn parse() {
         let path = "G:\\code\\suggest.js";
-        let file = File::open(path).unwrap();
         let comment_tokens = vec![CommentToken::Common("//".to_string())];
-        let mut parser = CommonParser::new(file, comment_tokens);
-        parser.parse();
-        println!("{}",parser);
+        let parser = CommonParser::new(path, comment_tokens);
+        let stats = parser.parse();
+        println!("{:#?}", stats);
     }
 }
